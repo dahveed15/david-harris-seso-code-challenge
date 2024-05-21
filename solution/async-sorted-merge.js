b377575192213a2121fabef461963087fa29a38a
dahveed15 <dahveed15@gmail.com>
2024-05-21 11:54:59 -0400

    Coding Challenge Complete

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -2,8 +2,52 @@
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
-module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
-  });
+module.exports = async (logSources, printer) => {
+
+  let lowestLogs = [];
+
+  //first step is to get first entry from each log source
+  //by doing log source by logsource.popAsync
+  for (let index = 0; index < logSources.length; index++) {
+    const logSource = logSources[index];
+
+    lowestLogs.push(await logSource.popAsync());
+  }
+
+  //while loop call until drained status are set to true
+  while (true) {
+    let lowestDateIdx = -1;
+
+    //compare dates and find the soonest one by date
+    for (let index = 0; index < lowestLogs.length; index++) {
+      const tempLog = lowestLogs[index];
+      const currentLowestLog = lowestLogs[lowestDateIdx];
+
+      if (!tempLog) {
+        continue;
+      }
+
+      if (!currentLowestLog || tempLog.date < currentLowestLog.date) {
+        lowestDateIdx = index;
+      }
+    }
+
+    //breaks while loop if all log sources are drained
+    if (lowestDateIdx === -1) {
+      break;
+    }
+
+    //on that log source, we call printer.print(log) on it
+    printer.print(lowestLogs[lowestDateIdx]);
+
+    //then we call logsource.popAsync on the one that was passed to printer.prints
+    lowestLogs[lowestDateIdx] = await logSources[lowestDateIdx].popAsync();
+  }
+
+  printer.done()
+
+
+
+  return console.log("Async sort complete.");
+
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -3,5 +3,50 @@
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+
+  let lowestLogs = [];
+
+  //first step is to get first entry from each log source
+  //by doing log source by logsource.pop
+  for (let index = 0; index < logSources.length; index++) {
+    const logSource = logSources[index];
+
+    lowestLogs.push(logSource.pop());
+  }
+
+  //while loop call until drained status are set to true
+  while (true) {
+    let lowestDateIdx = -1;
+
+    //compare dates and find the soonest one by date
+    for (let index = 0; index < lowestLogs.length; index++) {
+      const tempLog = lowestLogs[index];
+      const currentLowestLog = lowestLogs[lowestDateIdx];
+
+      if (!tempLog) {
+        continue;
+      }
+
+      if (!currentLowestLog || tempLog.date < currentLowestLog.date) {
+        lowestDateIdx = index;
+      }
+    }
+
+    //breaks while loop if all log sources are drained
+    if (lowestDateIdx === -1) {
+      break;
+    }
+
+    //on that log source, we call printer.print(log) on it
+    printer.print(lowestLogs[lowestDateIdx]);
+
+    //then we call logsource.pop on the one that was passed to printer.print
+    lowestLogs[lowestDateIdx] = logSources[lowestDateIdx].pop();
+  }
+
+  printer.done()
+
+
+
   return console.log("Sync sort complete.");
 };
